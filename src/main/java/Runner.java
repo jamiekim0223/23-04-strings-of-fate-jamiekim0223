80a8736a1528f45c824cc410c66749cca980639a
jamiekim0223 <jamiekim0223@gmail.com>
2022-11-03 09:11:16 -0500

    finished?

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,14 +1,19 @@
 public class Runner {
     public static void main(String[] args){
         System.out.println(StringySpells.middleFinder("balls"));
+        System.out.println("");
 
         System.out.println(StringySpells.alphaOmega("dog", "cat"));
+        System.out.println("");
 
         System.out.println(StringySpells.subtractive("woof", 7));
+        System.out.println("");
 
         System.out.println(StringySpells.flipper("send", "help"));
+        System.out.println("");
 
         System.out.println(StringySpells.aveLen("im", "begging"));
+        System.out.println("");
 
 
 
